452a86ff20dc3c6286e7a59c40a123154e223c74
dustbits <rbatista@protonmail.com>
2023-03-21 21:21:51 -0400

    Updated googletask.js name to mm-google-task.js

--- mm-google-task.js
+++ mm-google-task.js
@@ -1,4 +1,4 @@
-Module.register("MM-GoogleTask", {
+Module.register("mm-google-task", {
    defaults: {
       api_key: "",
       list_id: "",
@@ -11,7 +11,7 @@ Module.register("MM-GoogleTask", {
    },
 
    getStyles: function () {
-      return ["MM-GoogleTask.css"];
+      return ["mm-google-task.css"];
    },
 
    getTemplate: function () {
@@ -25,12 +25,12 @@ Module.register("MM-GoogleTask", {
    getData: function () {
       if (this.config.api_key !== "" && this.config.list_id !== "") {
          var url = `https://www.googleapis.com/tasks/v1/lists/${this.config.list_id}/tasks?key=${this.config.api_key}&maxResults=${this.config.max_results}`;
-         this.sendSocketNotification("MM-GoogleTask-GET_DATA", url);
+         this.sendSocketNotification("mm-google-task-GET_DATA", url);
       }
    },
 
    socketNotificationReceived: function (notification, payload) {
-      if (notification === "MM-GoogleTask-DATA_RECEIVED") {
+      if (notification === "mm-google-task-DATA_RECEIVED") {
          this.tasks = payload.items;
          this.updateDom();
       }
